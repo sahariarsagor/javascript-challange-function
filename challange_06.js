1e63ab5f003548715c59e2f610abd0cdafa28224
Sahariar Sagor <contactsahariar@gmail.com>
2024-08-03 21:26:06 +0600

    add code

--- challange_06.js
+++ challange_06.js
@@ -1,1 +1,15 @@
-console.log("sahariar sagor");
+function factorial(num){
+    //conditional statement 
+    if(num === 0){
+        console.log(1);
+    }
+
+    let result = 1; 
+    // let run a loop 
+    for(let i=1; i<=num; i++){
+        result*=i;
+    }
+    console.log(result);
+}
+
+factorial(5);
