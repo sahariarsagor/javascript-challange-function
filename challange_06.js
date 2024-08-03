805101507f4d656b063caed70b49b955cea7ad6e
Sahariar Sagor <contactsahariar@gmail.com>
2024-08-03 21:32:38 +0600

    add question and updated

--- challange_06.js
+++ challange_06.js
@@ -1,3 +1,10 @@
+/* Find the factorial of a number the logic of a factorial is: 
+    the factorial of 5 = 1*2*3*4*5
+    the factorial of 4 = 1*2*3*4
+this is the logic to calculate a factorial .     
+*/
+
+// Soution is: 
 function factorial(num){
     //conditional statement 
     if(num === 0){
