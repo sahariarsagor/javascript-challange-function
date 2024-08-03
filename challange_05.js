2ffbd87fa91bf537ba3e984d34b0e4d6874e4138
Sahariar Sagor <contactsahariar@gmail.com>
2024-08-03 15:40:53 +0600

    add question

--- challange_05.js
+++ challange_05.js
@@ -1,6 +1,13 @@
-/* is the  string is palindrome or not ?  */
+/* is the  string is palindrome or not ?  
+we check one string via function. the cleaned str and the reverse string is eqaul value and data types also than we say that 
+this string are palindrom. 
+as example: string is: ama; 
+cleaned string is = ama; 
+and reverse string is: ama;
+both are equal then we say its a palindrome. thank you here is the example 
+*/
 
-
+//solution of this problem: 
 function isPalindrome(str){
     let cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
     let reverseStr = cleanedStr.split('').reverse().join('');
